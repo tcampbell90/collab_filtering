db8b699ab73fd2b72552587d9446bba272730e22
tcampbell90 <thomas.campbell@nyu.edu>
2021-08-04 19:06:47 -0400

    replace deprecated pandas DataFrame method .ix with .iloc

--- collab_filter.py
+++ collab_filter.py
@@ -18,14 +18,14 @@ for i in range(0,len(data_ibs.columns)) :
     # Loop through the columns for each column
     for j in range(0,len(data_ibs.columns)) :
         # Fill in placeholder with cosine similarities
-        data_ibs.ix[i,j] = 1-cosine(data_germany.ix[:,i],data_germany.ix[:,j])
+        data_ibs.iloc[i,j] = 1-cosine(data_germany.iloc[:,i],data_germany.iloc[:,j])
 
 # Create a placeholder items for closes neighbours to an item
 data_neighbours = pd.DataFrame(index=data_ibs.columns,columns=[range(1,11)])
 
 # Loop through our similarity dataframe and fill in neighbouring item names
 for i in range(0,len(data_ibs.columns)):
-    data_neighbours.ix[i,:10] = data_ibs.ix[0:,i].order(ascending=False)[:10].index
+    data_neighbours.iloc[i,:10] = data_ibs.iloc[0:,i].order(ascending=False)[:10].index
 
 # --- End Item Based Recommendations --- #
 
@@ -37,7 +37,7 @@ def getScore(history, similarities):
 
 # Create a place holder matrix for similarities, and fill in the user name column
 data_sims = pd.DataFrame(index=data.index,columns=data.columns)
-data_sims.ix[:,:1] = data.ix[:,:1]
+data_sims.iloc[:,:1] = data.iloc[:,:1]
 
 #Loop through all rows, skip the user column, and fill with similarity scores
 for i in range(0,len(data_sims.index)):
@@ -45,22 +45,22 @@ for i in range(0,len(data_sims.index)):
         user = data_sims.index[i]
         product = data_sims.columns[j]
 
-        if data.ix[i][j] == 1:
-            data_sims.ix[i][j] = 0
+        if data.iloc[i][j] == 1:
+            data_sims.iloc[i][j] = 0
         else:
-            product_top_names = data_neighbours.ix[product][1:10]
-            product_top_sims = data_ibs.ix[product].order(ascending=False)[1:10]
-            user_purchases = data_germany.ix[user,product_top_names]
+            product_top_names = data_neighbours.iloc[product][1:10]
+            product_top_sims = data_ibs.iloc[product].order(ascending=False)[1:10]
+            user_purchases = data_germany.iloc[user,product_top_names]
 
-            data_sims.ix[i][j] = getScore(user_purchases,product_top_sims)
+            data_sims.iloc[i][j] = getScore(user_purchases,product_top_sims)
 
 # Get the top songs
 data_recommend = pd.DataFrame(index=data_sims.index, columns=['user','1','2','3','4','5','6'])
-data_recommend.ix[0:,0] = data_sims.ix[:,0]
+data_recommend.iloc[0:,0] = data_sims.iloc[:,0]
 
 # Instead of top song scores, we want to see names
 for i in range(0,len(data_sims.index)):
-    data_recommend.ix[i,1:] = data_sims.ix[i,:].order(ascending=False).ix[1:7,].index.transpose()
+    data_recommend.iloc[i,1:] = data_sims.iloc[i,:].order(ascending=False).iloc[1:7,].index.transpose()
 
 # Print a sample
-print data_recommend.ix[:10,:4]
+print(data_recommend.iloc[:10,:4])
